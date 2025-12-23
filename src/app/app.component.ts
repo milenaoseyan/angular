0c10736f12dc487a494d76caae5c065b675598d4
Осеян Милена <oseyan-milena@mail.ru>
2025-12-23 14:23:58 +0300

    Урок 3: TypeScript вычисления и интерполяция

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cnt-group';
+  title: string = 'Hello, Angular';
+  
+
+  operation: number = (15 + 7) * 3 - 4; 
 }
